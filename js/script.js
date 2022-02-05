466ecc3a2b62e95bd7732c9996893109c24b1cc5
karanbadhwar <karanbadhwar07@gmail.com>
2022-02-05 04:00:03 -0500

    Other-job-role-visibility

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Step - 1 Focus on INPUT Name Field
+const inputName = document.getElementById('name');
+inputName.focus();
+
+/*Step - 2 holding reference for <select> and Other Job roles into variable */
+const jobRole = document.getElementById('title');
+const otherJob = document.getElementById('other-job-role');
+
+otherJob.style.display = 'none';
+
+jobRole.addEventListener('change', e => {
+    const value = e.target.value;
+    if (value === 'other'){
+        otherJob.style.display = 'inline-block';
+    }
+});
