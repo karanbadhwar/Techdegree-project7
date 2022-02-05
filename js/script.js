423b92681e0d670460f6a762c1fb4212dbb527a3
karanbadhwar <karanbadhwar07@gmail.com>
2022-02-05 18:33:57 -0500

    Other-job-role
    Other-job-role works both ways if by mistake other-job gets clicked and if changed again other-job-role gets hidden again

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-// Step - 1 Focus on INPUT Name Field
+// Step - 3 Focus on INPUT Name Field
 const inputName = document.getElementById('name');
 inputName.focus();
 
-/*Step - 2 holding reference for <select> and Other Job roles into variable */
+/*Step - 4 holding reference for <select> and Other Job roles into variable */
 const jobRole = document.getElementById('title');
 const otherJob = document.getElementById('other-job-role');
 
 otherJob.style.display = 'none';
 
+//Step-5 changing the other-job-role Input as per the the Option selected from the JOb Role Section
 jobRole.addEventListener('change', e => {
     const value = e.target.value;
     if (value === 'other'){
         otherJob.style.display = 'inline-block';
+    }
+    else if (value !== 'other'){
+        otherJob.style.display = 'none';
     }
 });
